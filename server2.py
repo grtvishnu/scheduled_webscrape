e758939c206293ccf64e38a9f207f57400f4664e
Vishnu Unnikrishnan <optra7@gmail.com>
2020-06-08 18:54:31 +0530

    add logger

--- server2.py
+++ server2.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from scarpe import run as scrape_runner
-
+from logger import trigger_log_save
 app = FastAPI()
 
 
@@ -12,4 +12,5 @@ def hellow():
 @app.post("/box-office-mojo-scraper")
 def boxpffice_scrape_view():
     scrape_runner()
+    trigger_log_save()
     return{"data": [1, 2, 3]}
--- trigger.py
+++ trigger.py
@@ -1,7 +1,7 @@
 import requests
 
 ngrok_url = 'http://4eab58c6824b.ngrok.io'
-endpoint = f'{ngrok_url}/box-office-mojo-scraper'
+endpoint = f'{ngrok_url}/ '
 
 r = requests.post(endpoint, json={})
 print(r.json()['data'])
